f0181c02499e88ce217af2d81a5634d31be4eee3
Ryszol <ryszol@protonmail.com>
2020-09-20 21:23:10 -0400

    Design previamente pronto.

---
+++ script.js
@@ -0,0 +1,53 @@
+const quiz = [
+  {
+    'enunciated': '1. Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'alternatives': {
+      'a': 'Tem entre 2 a 4 litros. São retirados 450 mililitros',
+      'b': 'Tem entre 4 a 6 litros. São retirados 450 mililitros',
+      'c': 'Tem 10 litros. São retirados 2 litros',
+      'd': 'Tem 7 litros. São retirados 1,5 litros',
+      'e': 'Tem 0,5 litros. São retirados 0,5 litros'
+    },
+    'answer': 'b'
+  }, {
+    'enunciated': '2. De quem é a famosa frase “Penso, logo existo”?',
+    'alternatives': {
+      'a': 'Platão',
+      'b': 'Galileu Galilei',
+      'c': 'Descartes',
+      'd': 'Sócrates',
+      'e': 'Francis Bacon'
+    },
+    'answer': 'c'
+  }, {
+    'enunciated': '3. De onde é a invenção do chuveiro elétrico?',
+    'alternatives': {
+      'a': 'França',
+      'b': 'Inglaterra',
+      'c': 'Brasil',
+      'd': 'Austrália',
+      'e': 'Itália'
+    },
+    'answer': 'c'
+  }, {
+    'enunciated': '4. Quais o menor e o maior país do mundo?',
+    'alternatives': {
+      'a': 'Vaticano e Rússia',
+      'b': 'Nauru e China',
+      'c': 'Mônaco e Canadá',
+      'd': 'Malta e Estados Unidos',
+      'e': 'São Marino e Índia'
+    },
+    'answer': 'a'
+  }, {
+    'enunciated': '5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'alternatives': {
+      'a': 'Jânio Quadros',
+      'b': 'Jacinto Anjos',
+      'c': 'Getúlio Vargas',
+      'd': 'João Figueiredo',
+      'e': 'João Goulart'
+    },
+    'answer': 'e'
+  },
+]
